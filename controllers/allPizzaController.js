9e6b5ae4dcb97c627e0f9ad9a7ab9da7cb1d4348
subhakanta roul <papu.roul99@gmail.com>
2023-04-28 00:13:55 +0530

    PIZ-18: modified all pizza controller - removed search pizza functionality

--- controllers/allPizzaController.js
+++ controllers/allPizzaController.js
@@ -1,22 +1,16 @@
-const pizzaModel = require("../models/pizza");
+const pizzaModel = require('../models/pizza');
 const allPizzaController = async (req, res) => {
     try {
-        let pizzas;
-        const query = new RegExp(req.query.q, 'ig')
-        if(query){
-            pizzas = await pizzaModel.find({"name": query});
-        } else{
-            pizzas = await pizzaModel.find({});
-        }
-        
+        const pizzas = await pizzaModel.find({});
+
         if (pizzas) {
             res.send(pizzas);
         } else {
-            console.log("error fetching pizzas");
+            console.log('error fetching pizzas');
         }
     } catch (err) {
         console.log(err);
     }
-}
+};
 
 module.exports = allPizzaController;
