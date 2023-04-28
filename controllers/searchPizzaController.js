ecc6b3da55e9107e60fddf30fee520f28f428331
subhakanta roul <papu.roul99@gmail.com>
2023-04-28 20:58:21 +0530

    PIZ-18: api implementation for search pizza functionality

--- controllers/searchPizzaController.js
+++ controllers/searchPizzaController.js
@@ -1,5 +1,43 @@
-const searchPizzaController = (req, res) => {
-    res.status(200).json({msg: "search pizza controller working!"})
+const pizzaModel = require('../models/pizza');
+
+/**
+ *
+ * @param {*} req request object
+ * @param {*} res response object
+ * @returns
+ * @description this method returns an array of pizzas based on the search term.
+ */
+const searchPizzaController = async (req, res) => {
+    try {
+        let searchTerm = req.query.q.trim();
+        if (searchTerm) {
+            searchTerm = new RegExp(searchTerm, 'ig');
+            const pizzas = await pizzaModel.find({ name: searchTerm });
+            if (pizzas.length > 0) {
+                res.status(200).json({
+                    status: 'success',
+                    message: 'pizzas found successfully',
+                    data: pizzas,
+                });
+            } else {
+                res.status(200).json({
+                    status: 'success',
+                    message: 'no pizzas found',
+                });
+            }
+        } else {
+            res.status(400).json({
+                status: 'failure',
+                message: 'please enter something in the search field',
+            });
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            status: 'failure',
+            message: 'internal server error',
+        });
+    }
 };
 
 module.exports = searchPizzaController;
