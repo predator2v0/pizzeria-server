81a8e43ed1cdc44772475f12791172775a1a93db
subhakanta roul <papu.roul99@gmail.com>
2023-04-28 23:07:22 +0530

    PIZ-18: get all pizza api modification

--- controllers/allPizzaController.js
+++ controllers/allPizzaController.js
@@ -1,15 +1,31 @@
 const pizzaModel = require('../models/pizza');
+
+/**
+ * @param {*} req request object
+ * @param {*} res response object
+ * @description this method returns all the pizzas from the database.
+ */
 const allPizzaController = async (req, res) => {
     try {
         const pizzas = await pizzaModel.find({});
-
         if (pizzas) {
-            res.send(pizzas);
+            res.status(200).json({
+                status: 'success',
+                message: 'pizzas found successfully',
+                data: pizzas,
+            });
         } else {
-            console.log('error fetching pizzas');
+            res.status(400).json({
+                status: 'failure',
+                message: 'pizzas not found',
+            });
         }
     } catch (err) {
         console.log(err);
+        res.status(500).json({
+            status: 'failure',
+            message: 'internal server error',
+        });
     }
 };
 
