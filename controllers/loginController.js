cededd216ad87b7e6eee5c8379cac336a4aa933a
subhakanta roul <papu.roul99@gmail.com>
2023-04-29 00:03:46 +0530

    PIZ-18: login controller modifications

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,15 +1,25 @@
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const user = require("../models/user");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
+const user = require('../models/user');
+
+/**
+ * @param {*} req request object
+ * @param {*} res response object
+ * @description this method returns response status code and the login status.
+ * i.e. if the login is successful or not. if successful then this method returns a token
+ * for the login session.
+ */
 const loginController = async (req, res) => {
     const { email, password } = req.body;
     try {
         if (!email || !password) {
-            return res.status(400).json({ msg: "please fill all the fields" });
+            return res.status(400).json({
+                status: 'failure',
+                message: 'please fill all the fields',
+            });
         }
         const userExists = await user.findOne({ email });
-        console.log(email, password);
         if (
             userExists &&
             userExists.email === email &&
@@ -21,22 +31,25 @@ const loginController = async (req, res) => {
             };
             // create and sign JWT auth token for validation
             const authToken = jwt.sign(userAuthData, process.env.AUTH_KEY, {
-                expiresIn: "1h",
+                expiresIn: '1h',
             });
-            return res.status(200).json({
-                msg: "user logged in successfully",
-                authToken
+            res.status(200).json({
+                status: 'success',
+                message: 'login successful',
+                token: authToken,
             });
         } else {
-            return res.status(400).json({ msg: "incorrect credentials" });
+            res.status(401).json({
+                status: 'failure',
+                message: 'incorrect credentials',
+            });
         }
     } catch (err) {
-        return res
-            .status(400)
-            .json({
-                err: err.toString(),
-                msg: "Login Failed! please try again!",
-            });
+        console.log(err);
+        res.status(500).json({
+            status: 'failure',
+            message: 'internal server error',
+        });
     }
 };
 
