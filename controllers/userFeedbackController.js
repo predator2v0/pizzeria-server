8891361df1b419dd78b434484b4968fc7050af5f
subhakanta roul <papu.roul99@gmail.com>
2023-04-29 15:28:23 +0530

    PIZ-18: user feedback controller update

--- controllers/userFeedbackController.js
+++ controllers/userFeedbackController.js
@@ -1,38 +1,56 @@
-const feedbackModel = require("../models/feedback");
+const feedbackModel = require('../models/feedback');
 
+/**
+ * @param {*} req request object
+ * @param {*} res response object
+ * @description handles the user feedback. when triggered this controller, it checks for the
+ * email if it's present in the db, inserts the feedback in that document otherwise creates
+ * another document and inserts the user's feedback as a fresh record.
+ */
 const userFeedbackController = async (req, res) => {
     try {
-        const email = req.body.email;
-        const feedback = req.body.feedback;
-        if (!email || !feedback) {
-            res.status(400).json({ msg: "please fill all the fields" });
+        const { email, feedback } = req.body;
+        let msg = '';
+        if (!email.trim() || !feedback.trim()) {
+            res.status(400).json({
+                status: 'failure',
+                message: 'please fill all the mandatory fields',
+            });
         } else {
             const emailExists = await feedbackModel.findOne({ email });
-
             if (emailExists) {
                 const feedbackUpdated = await feedbackModel.updateOne(
                     { email },
                     { $push: { feedback } }
                 );
                 feedbackUpdated
-                    ? (msg = "feedback updated")
-                    : (msg = "unable to update feedback");
+                    ? (msg = 'feedback updated')
+                    : (msg = 'unable to update feedback');
             } else {
                 const feedbackInserted = await feedbackModel.create({
                     email,
                     feedback,
                 });
                 feedbackInserted
-                    ? (msg = "feedback inserted")
-                    : (msg = "unable to insert feedback");
+                    ? (msg = 'feedback inserted')
+                    : (msg = 'unable to insert feedback');
             }
+            const feedbackData = {
+                email: email,
+                feedback: feedback,
+            };
+            res.status(201).json({
+                status: 'success',
+                message: msg,
+                data: feedbackData,
+            });
         }
-        // !test comment
-        console.log(email, feedback, msg);
-        res.status(200).json({ email, feedback, msg });
     } catch (err) {
         console.log(err);
-        res.status(400).json({ msg: "error occured" + err });
+        res.status(500).json({
+            status: 'failure',
+            message: 'internal server error',
+        });
     }
 };
 
