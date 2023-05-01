7a795e96be6e19fc26a40e7e65f7faf4e733d9f5
subhakanta roul <papu.roul99@gmail.com>
2023-05-01 22:01:25 +0530

    PIZ-19: doc update for the place order controller and utility methods.

--- controllers/placeOrderController.js
+++ controllers/placeOrderController.js
@@ -1,6 +1,12 @@
 const orderModel = require('../models/order');
-const { v1: uuid } = require('uuid');
 
+/**
+ * @param {*} req request object
+ * @param {*} res response object
+ * @description this mehtod is used to place an order. i.e. get the order content from client,
+ * append the order timestamp and order id tothe order and store the order into
+ * the order collection in the DB.
+ */
 const placeOrderController = async (req, res) => {
     try {
         const userId = req.params.userId;
@@ -24,12 +30,12 @@ const placeOrderController = async (req, res) => {
         if (placeOrder) {
             const orderData = {
                 orderId: placeOrder.orderID,
-                timeStamp: placeOrder.timeStamp
-            }
+                timeStamp: placeOrder.timeStamp,
+            };
             res.status(201).json({
                 status: 'success',
                 message: 'order placed successfully',
-                data: orderData
+                data: orderData,
             });
         } else {
             res.status(500).json({
@@ -46,6 +52,14 @@ const placeOrderController = async (req, res) => {
     }
 };
 
+/**
+ * @returns {string} orderId
+ * @description this method generates a order id based on the previous order.
+ * it checks in the database what is the order id of last order and generates
+ * an order id with the next number.
+ * the order id is a 10 char long string starting with ORD and next 7 places are
+ * filled with the successive number of the last order.
+ */
 const generateOrderId = async () => {
     const lastOrder = await orderModel.findOne(
         {},
@@ -56,6 +70,13 @@ const generateOrderId = async () => {
     const orderId = `ORD${(lastSl + 1).toString().padStart(7, 0)}`;
     return orderId;
 };
+
+/**
+ * @returns {string} current timestamp in string format
+ * @description this method generated the current time stamp in en-IN format
+ * and returns the timestamp in string format. the timestamp for each order is
+ * appended to the order.
+ */
 const generateOrderTimeStamp = () => {
     return new Date().toLocaleString('en-IN').toString();
 };
