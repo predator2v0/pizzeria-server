02c50b9178ad1db33b1a387f5f4be8a92a54ebbd
subhakanta roul <papu.roul99@gmail.com>
2023-05-04 22:43:35 +0530

    PIZ-20: WIP for update profile api

--- controllers/updateProfileController.js
+++ controllers/updateProfileController.js
@@ -1,5 +1,24 @@
+const userModel = require('../models/user');
+
 const updateProfileController = (req, res) => {
-    res.status(200).json({msg: "update profile controller working!"})
+    try{
+        const userId = req.params.userId;
+        const {name, address, state, pincode} = req.body;
+        console.log("userId", userId);
+        console.log("name", name);
+        console.log("address", address);
+        console.log("state", state);
+        console.log("pincode", pincode);
+
+
+        res.status(200).json({message: "update profile controller working!"})
+    } catch(err){
+        console.log(err);
+        res.status(500).json({status:'failure', message: 'internal server error'})
+    }
 };
 
 module.exports = updateProfileController;
+
+
+// *WIP: update profile under progress, to be continued from here.
