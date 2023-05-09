e586748e596001df61cc7e4f270123e2004121c0
subhakanta roul <papu.roul99@gmail.com>
2023-05-09 22:39:45 +0530

    PIZ-22: auth middleware update

--- middlewares/auth/verifyToken.js
+++ middlewares/auth/verifyToken.js
@@ -1,8 +1,16 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
+/**
+ * @param {*} req request object
+ * @param {*} res response object
+ * @param {*} next next function to be executed after the middleware
+ * @description this middleware function is responsible for api authentication.
+ * it checks for the auth token sent along with the secure api request header.
+ * if it matches the token at the token in the server, the control goes to the
+ * corresponding api controller, else the request is blocked.
+ */
 function verifyToken(req, res, next) {
     const authToken = req.headers.authorization;
-    console.log(req.headers);
     if (authToken) {
         // * verify token
         const tokenVerified = jwt.verify(
@@ -10,15 +18,20 @@ function verifyToken(req, res, next) {
             process.env.AUTH_KEY,
             (err, data) => {
                 if (err) {
-                    res.status(400).json({ msg: "invalid token" });
+                    res.status(403).json({
+                        status: 'failure',
+                        message: 'Unauthorized! invalid token',
+                    });
                 } else {
-                    // console.log(data);
                     next();
                 }
             }
         );
     } else {
-        res.status(400).json({ msg: "token not found" });
+        res.status(401).json({
+            status: 'failure',
+            message: 'Unauthorized! token not found',
+        });
     }
 }
 
